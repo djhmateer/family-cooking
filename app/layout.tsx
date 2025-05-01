64b61ff72d3f6b8851b36da8fb66e591961325d9
Dave Mateer <davemateer@gmail.com>
2025-05-01 14:52:08 +0100

    moved globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
+// import "@/assets/styles/globals.css";
+import "@/app/globals.css";
 
 import { UserIcon, CookingPot, Info, Bug } from "lucide-react";
 import Image from "next/image";
