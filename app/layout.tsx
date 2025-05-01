492fefa5eae5bb3523390322dc24428d9af24ba8
Dave Mateer <davemateer@gmail.com>
2025-05-01 14:27:47 +0100

    patch in wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {/* 
+          h-screen: sets height to 100vh (full viewport height)
+          flex-col: sets flex direction to column (stacks children vertically) */}
+        <div className="flex h-screen flex-col">
+          {/* <Header /> */}
+
+          {/* flex-1: allows this element to grow and take up remaining space
+            which creates a sticky footer
+            wrapper: custom class in globals.css, not a Tailwind class which does:
+           <main className="flex-1 max-w-7xl lg:mx-auto p-5 md:px-10 w-full"> */}
+          <main className="flex-1 wrapper">{children}</main>
+
+          {/* <Footer /> */}
+        </div>
+      </body>
     </html>
   );
 }
