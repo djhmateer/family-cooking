a550aeafb53c9f2711656b1fb349ef5d443529f1
Dave Mateer <davemateer@gmail.com>
2025-05-01 14:21:12 +0100

    put in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "@/assets/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Family-Cooking Recipes",
+  description: "A modern family cooking site",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
