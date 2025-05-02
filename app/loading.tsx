c500e1a86b9f510f88be2c24f9b419978d53f949
Dave Mateer <davemateer@gmail.com>
2025-05-02 07:12:44 +0100

    loading page test with image in assets

---
+++ app/loading.tsx
@@ -0,0 +1,26 @@
+import loader from "@/assets/loader.gif";
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Image
+        src={loader}
+        width={100}
+        height={100}
+        alt="Loading..."
+        unoptimized
+      />
+    </div>
+  );
+};
+
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-const HomePage = () => {
-  return ( 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const HomePage = async () => {
+  await delay(2000);
+  return (
     <div>
       <h1>Hello World!</h1>
     </div>
   );
-}
- 
-export default HomePage;
+};
 
+export default HomePage;
