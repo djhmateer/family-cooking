6fdd5795cf8a0898698049efefbaec9da570f6d8
Dave Mateer <davemateer@gmail.com>
2025-05-02 14:43:54 +0100

    go pure client side for home page ie pre built and cached data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-export const dynamic = "force-dynamic";
-
 import sampleData from "@/db/traversy-sample-data";
 
-// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const HomePage = () => {
-  // await delay(2000);
   console.log("log data is ", sampleData);
 
   return (
